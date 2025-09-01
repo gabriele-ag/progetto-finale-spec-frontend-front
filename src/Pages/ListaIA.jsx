020984f3303dad5ace6a365b6ce817d53623fe87
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-09-01 10:04:10 +0200

    feat: created option filter for category

--- src/Pages/ListaIA.jsx
+++ src/Pages/ListaIA.jsx
@@ -14,11 +14,12 @@ const ListaIA = () => {
 
     const filteredAI = useMemo(() => {
         return listAI.filter((elem) => {
-            const matchText = elem.title.toLowerCase().includes(search.toLowerCase())
+            const matchText = elem.title.toLowerCase().includes(search.toLowerCase()) 
+            const matchSelectedCategory = selectedCategory === "" || elem.category.trim().toLowerCase() === selectedCategory.trim().toLowerCase()
 
-            return matchText
+            return matchText && matchSelectedCategory
         })
-    }, [listAI, search])
+    }, [listAI, search, selectedCategory])
 
 
 
@@ -26,8 +27,9 @@ const ListaIA = () => {
         <>
 
             <main>
+
                 {/* Input per i filtri */}
-                <form action="" onSubmit={handleForm}>
+                <form onSubmit={handleForm}>
 
                     <input 
                     type="text"
@@ -41,18 +43,31 @@ const ListaIA = () => {
                     >
                         <option value="">Tutte le categorie</option>
                         <option value="Generazione testo">Generazione testo</option>
-                        <option value="Video IA">Video IA</option>
+                        <option value="Generazione Immagini">Generazione Immagini</option>
+                        <option value="Video AI">Video AI</option>
                         <option value="Copywriting">Copywriting</option>
                         <option value="Design Assistito">Design Assistito</option>
+                        <option value="Produttività">Produttività</option>
+                        <option value="Correzione Testi">Correzione Testi</option>
+                        <option value="Video Editing AI">Video Editing AI</option>
+                        <option value="Video da Testo">Video da Testo</option>
+                        <option value="Audio/Video Editing">Audio/Video Editing</option>
+                        <option value="Presentazioni">Presentazioni</option>
+                        <option value="SEO AI">SEO AI</option>
+                        <option value="Ricerca AI">Ricerca AI</option>
+                        <option value="Parafrasi">Parafrasi</option>
+
                     </select>
 
-                    <input type="text" />
                 </form>
 
                 {/* Elenco delle IA */}
                 <ul>
                     {filteredAI.map((curList) => (
-                        <li key={curList.id}>{curList.title}</li>
+                        <li key={curList.id}>
+                            <h2>{curList.title}</h2>
+                            <p>{curList.category}</p>
+                        </li>
                     ))}
                 </ul>
 
